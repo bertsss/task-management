38808de11dd1720c8580ec937911dd23622f1ebf
Robert Bonifacio Jr <robert.taisondigital@gmail.com>
2025-04-21 10:12:00 +0800

    Added vue-router package and initial apply side navigation type on UI

---
+++ src/components/SideNav.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+</script>
+<template>
+    <div class="min-h-screen">
+        <h1 class="text-3xl font-bold underline">Side Nav</h1>
+    </div>
+</template>
---
+++ src/pages/ViewTask.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>View task page</h1>
+</template>
