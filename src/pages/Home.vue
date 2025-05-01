665bc004ce177b458cd49033998bf36f8bb46392
bertsss <berttsss@gmail.com>
2025-05-01 13:13:45 +0800

    Create initial home component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>
-        <h1>Home</h1>
+    <div class="p-6 bg-neutral-100 h-full">
+        <h2 class="text-3xl text-slate-600 mb-6">List of all tasks</h2>
+        <div class="w-full h-[200px] rounded-2xl shadow-sm bg-white">
+        </div>
     </div>
 </template>
